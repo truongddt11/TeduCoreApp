71825b6291dff33dc4f69676daf3932b5cdc3f72
Dong Dao Truong <truongdd11@gmail.com>
2020-08-27 15:43:56 +0700

    Tao cac Entity de sinh ra CSDL

---
+++ TeduCoreApp.Data.EF/Configurations/TagConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TeduCoreApp.Data.EF.Extensions;
+using TeduCoreApp.Data.Entities;
+
+namespace TeduCoreApp.Data.EF.Configurations
+{
+    public class TagConfiguration : DbEntityConfiguration<Tag>
+    {
+        public override void Configure(EntityTypeBuilder<Tag> entity)
+        {
+            entity.Property(c => c.Id).HasMaxLength(50)
+                .IsRequired().HasColumnType("varchar(50)");
+        }
+    }
+}
---
+++ TeduCoreApp.Data/Entities/ProductTag.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using TeduCoreApp.Infrastructure.SharedKernel;
+
+namespace TeduCoreApp.Data.Entities
+{
+    public class ProductTag : DomainEntity<int>
+    {
+        public int ProductId { get; set; }
+
+        [StringLength(50)]
+        [Column(TypeName = "varchar")]
+        public string TagId { set; get; }
+
+        [ForeignKey("ProductId")]
+        public virtual Product Product { set; get; }
+
+        [ForeignKey("TagId")]
+        public virtual Tag Tag { set; get; }
+    }
+}
